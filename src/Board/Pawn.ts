ade8402d1f89468842d68613ffa83448667e455c
tkrzak <tomekjkrzak@gmail.com>
2021-01-29 18:59:53 +0100

    Remove toBeCaptured proberty from Pieces

--- src/Board/Pawn.ts
+++ src/Board/Pawn.ts
@@ -4,10 +4,20 @@ import { PieceType } from "./PieceTypeEnum";
 import { Piece } from "./Piece";
 
 export class Pawn extends Piece {
-    constructor(pieceColor: PieceColor, pieceType: PieceType, placeAt: Square, moved: boolean, toBeCaptured: boolean) {
-        super(pieceColor, pieceType, placeAt, moved, toBeCaptured);
-    }
+  constructor(
+    pieceColor: PieceColor,
+    pieceType: PieceType,
+    placeAt: Square,
+    moved: boolean
+  ) {
+    super(pieceColor, pieceType, placeAt, moved);
+  }
 
-    public validMoves(): void /*List<Square>*/ {/*TO DO*/ };
-    public makeMove(): void /*Move*/ {/*TO DO*/ };
+  public validMoves(): void /*List<Square>*/ {
+    /*TO DO*/
+  }
+
+  public makeMove(): void /*Move*/ {
+    /*TO DO*/
+  }
 }
--- src/Board/Queen.ts
+++ src/Board/Queen.ts
@@ -4,9 +4,18 @@ import { PieceType } from "./PieceTypeEnum";
 import { Piece } from "./Piece";
 
 export class Queen extends Piece {
-    constructor(pieceColor: PieceColor, pieceType: PieceType, placeAt: Square, moved: boolean, toBeCaptured: boolean) {
-        super(pieceColor, pieceType, placeAt, moved, toBeCaptured);
-    }
-    public validMoves(): void /*List<Square>*/ {/*TO DO*/ };
-    public makeMove(): void /*Move*/ {/*TO DO*/ };
+  constructor(
+    pieceColor: PieceColor,
+    pieceType: PieceType,
+    placeAt: Square,
+    moved: boolean
+  ) {
+    super(pieceColor, pieceType, placeAt, moved);
+  }
+  public validMoves(): void /*List<Square>*/ {
+    /*TO DO*/
+  }
+  public makeMove(): void /*Move*/ {
+    /*TO DO*/
+  }
 }
