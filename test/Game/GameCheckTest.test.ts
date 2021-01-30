546ac6181aaae30514456b9fec1eadc8560d285f
Radek <radexw1@gmail.com>
2021-01-30 21:17:36 +0100

    fix: update check positions

--- test/Game/GameCheckTest.test.ts
+++ test/Game/GameCheckTest.test.ts
@@ -12,6 +12,109 @@ import { Game } from "../../src/Game/Game";
 describe("TESTS FOR CHECK", () => {
 
     //TEST 1//
+    test("check if black King is checked on position [6][6]", () => {
+      const newBoard = new Board();
+  
+      const whiteKingPosition = new Square(5, 2);
+      const whiteQueenPosition = new Square(4, 4);
+      const whiteRookPosition = new Square(2, 3);
+      const blackKingPosition = new Square(6, 6);
+      const blackKnightPosition = new Square(6, 1);
+  
+      const whiteKing = new King(
+        PieceColor.White,
+        PieceType.King,
+        whiteKingPosition
+      );
+  
+      const whiteQueen = new Pawn(
+        PieceColor.White,
+        PieceType.Queen,
+        whiteQueenPosition
+      );
+  
+      const whiteRook = new Pawn(
+        PieceColor.White,
+        PieceType.Rook,
+        whiteRookPosition
+      );
+  
+      const blackKing = new King(
+        PieceColor.Black,
+        PieceType.King,
+        blackKingPosition
+      );
+  
+      const blackKnight = new Knight(
+        PieceColor.Black,
+        PieceType.Knight,
+        blackKnightPosition
+      );
+  
+      newBoard.addPiece(whiteKing);
+      newBoard.addPiece(whiteQueen);
+      newBoard.addPiece(whiteRook);
+      newBoard.addPiece(blackKing);
+      newBoard.addPiece(blackKnight);
+  
+      const newGame = new Game(
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        newBoard
+      );
+  
+      const result = newGame.isChecked();
+      expect(result).toBe(true);
+    });
+
+  //TEST 2//
+
+  test("check if white King is checked on position [5][2]", () => {
+    const newBoard = new Board();
+
+    const whiteKingPosition = new Square(5, 2);
+    const blackKingPosition = new Square(7, 5);
+    const whiteRookPosition = new Square(6, 1);
+
+    const whiteKing = new King(
+      PieceColor.White,
+      PieceType.King,
+      whiteKingPosition
+    );
+
+    const blackKing = new King(
+      PieceColor.Black,
+      PieceType.King,
+      blackKingPosition
+    );
+
+    const whiteRook = new Knight(
+      PieceColor.White,
+      PieceType.Rook,
+      whiteRookPosition
+    );
+
+    newBoard.addPiece(whiteKing);
+    newBoard.addPiece(blackKing);
+    newBoard.addPiece(whiteRook);
+
+    const newGame = new Game(
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      newBoard
+    );
+
+    const result = newGame.isChecked();
+    expect(result).toBe(true);
+  });
+
+  //TEST 3//
 
   test("check if white King is checked on position [3][4]", () => {
 
@@ -45,145 +148,16 @@ describe("TESTS FOR CHECK", () => {
     );
 
     const result = newGame.isChecked();
-    expect(result).toBe(true);
-  });
-
-  //TEST 2//
-
-  test("check if white King is checked on position [5][2]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(5, 2);
-    const blackKingPosition = new Square(7, 5);
-    const blackKnightPosition = new Square(6, 1);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isChecked();
-    expect(result).toBe(true);
-  });
-
-  //TEST 3//
-
-  test("check if white King is checked on position [0][0]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(0, 0);
-    const whiteBishopPosition = new Square(1, 1);
-
-    const blackKingPosition = new Square(7, 5);
-    const blackKnightPosition = new Square(6, 1);
-    const blackRookPosition = new Square(3, 1);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const whiteBishop = new Bishop(
-      PieceColor.White,
-      PieceType.Bishop,
-      whiteBishopPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    const blackRook = new Rook(
-      PieceColor.Black,
-      PieceType.Rook,
-      blackRookPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteBishop);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-    newBoard.addPiece(blackRook);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isChecked();
     expect(result).toBe(false);
   });
 
   //TEST 4//
 
-  test("check if white King is checked on position [2][2]", () => {
+  test("check if black King is checked on position [2][2]", () => {
     const newBoard = new Board();
-
-    const whiteKingPosition = new Square(2, 2);
-    const whiteBishopPosition = new Square(3, 4);
-    const whitePawnPosition = new Square(7, 8);
-
-    const blackKingPosition = new Square(7, 5);
-    const blackKnightPosition = new Square(6, 1);
-    const blackRookPosition = new Square(3, 1);
-    const blackQueenPosition = new Square(5, 5);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const whiteBishop = new Bishop(
-      PieceColor.White,
-      PieceType.Bishop,
-      whiteBishopPosition
-    );
-
-    const whitePawn = new Pawn(
-      PieceColor.White,
-      PieceType.Pawn,
-      whitePawnPosition
-    );
+    const whiteKingPosition = new Square(7, 4);
+    const whiteRookPosition = new Square(6, 2);
+    const blackKingPosition = new Square(2, 2);
 
     const blackKing = new King(
       PieceColor.Black,
@@ -191,31 +165,21 @@ describe("TESTS FOR CHECK", () => {
       blackKingPosition
     );
 
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
+    const whiteKing = new King(
+      PieceColor.White,
+      PieceType.King,
+      whiteKingPosition
     );
 
-    const blackRook = new Rook(
-      PieceColor.Black,
+    const whiteRook = new Pawn(
+      PieceColor.White,
       PieceType.Rook,
-      blackRookPosition
-    );
-
-    const blackQueen = new Rook(
-      PieceColor.Black,
-      PieceType.Queen,
-      blackQueenPosition
+      whiteRookPosition
     );
 
     newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteBishop);
-    newBoard.addPiece(whitePawn);
+    newBoard.addPiece(whiteRook);
     newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-    newBoard.addPiece(blackRook);
-    newBoard.addPiece(blackQueen);
 
     const newGame = new Game(
       undefined,
@@ -277,42 +241,22 @@ describe("TESTS FOR CHECK", () => {
 
   //TEST 6//
 
-  test("check if black King is checked on position [6][3]", () => {
+  test("check if black King is checked on position [0][7]", () => {
     const newBoard = new Board();
+    const whiteKingPosition = new Square(7, 4);
+    const whiteQueenPosition = new Square(5, 2);
+    const blackKingPosition = new Square(0, 7);
 
-    const whiteKingPosition = new Square(2, 2);
-    const whiteBishopPosition = new Square(3, 4);
-    const whitePawnPosition = new Square(7, 8);
-    const whiteRookPosition = new Square(3, 3);
-    const whiteQueenPosition = new Square(4, 4);
-
-    const blackKingPosition = new Square(6, 3);
-    const blackKnightPosition = new Square(6, 1);
-    const blackRookPosition = new Square(6, 4);
-    const blackQueenPosition = new Square(6, 5);
+    const blackKing = new King(
+      PieceColor.Black,
+      PieceType.King,
+      blackKingPosition
+    );
 
     const whiteKing = new King(
       PieceColor.White,
       PieceType.King,
       whiteKingPosition
-    );
-
-    const whiteBishop = new Bishop(
-      PieceColor.White,
-      PieceType.Bishop,
-      whiteBishopPosition
-    );
-
-    const whitePawn = new Pawn(
-      PieceColor.White,
-      PieceType.Pawn,
-      whitePawnPosition
-    );
-
-    const whiteRook = new Pawn(
-      PieceColor.White,
-      PieceType.Rook,
-      whiteRookPosition
     );
 
     const whiteQueen = new Pawn(
@@ -321,39 +265,9 @@ describe("TESTS FOR CHECK", () => {
       whiteQueenPosition
     );
 
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    const blackRook = new Rook(
-      PieceColor.Black,
-      PieceType.Rook,
-      blackRookPosition
-    );
-
-    const blackQueen = new Rook(
-      PieceColor.Black,
-      PieceType.Queen,
-      blackQueenPosition
-    );
-
     newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteBishop);
-    newBoard.addPiece(whitePawn);
-    newBoard.addPiece(whiteRook);
     newBoard.addPiece(whiteQueen);
     newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-    newBoard.addPiece(blackRook);
-    newBoard.addPiece(blackQueen);
 
     const newGame = new Game(
       undefined,
@@ -365,7 +279,7 @@ describe("TESTS FOR CHECK", () => {
     );
 
     const result = newGame.isChecked();
-    expect(result).toBe(false);
+    expect(result).toBe(true);
   });
 
   //TEST 7//
@@ -425,7 +339,7 @@ describe("TESTS FOR CHECK", () => {
     );
 
     const result = newGame.isChecked();
-    expect(result).toBe(false);
+    expect(result).toBe(true);
   });
 
   //TEST 8//
@@ -462,4 +376,4 @@ describe("TESTS FOR CHECK", () => {
     const result = newGame.isChecked();
     expect(result).toBe(false);
   });
-});
+})
