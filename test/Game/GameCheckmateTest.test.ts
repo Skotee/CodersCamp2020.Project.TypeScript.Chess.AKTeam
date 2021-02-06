5d89c72234fe62f5300bf7a1987f059f426c5d72
Chlip <bialas.maciej@gmail.com>
2021-02-07 00:32:49 +0100

    poprawilem testy sprawdzajace czy jest mat

--- test/Game/GameCheckmateTest.test.ts
+++ test/Game/GameCheckmateTest.test.ts
@@ -9,454 +9,87 @@ import { Rook } from "../../src/Board/Rook";
 import { Queen } from "../../src/Board/Queen";
 import { Square } from "../../src/Board/Square";
 import { Game } from "../../src/Game/Game";
+import { Move } from "../../src/Game/Move";
 
 describe("TESTS FOR CHECKEDMATE", () => {
   //TEST 1//
 
-  test("check white King checkmate on position [3][4]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(3, 4);
-    const blackKingPosition = new Square(7, 5);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(blackKing);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(true);
+  test("BLACK KING CHECKMATE BY ROOK AND WHITE KING", () => {
+    //given
+    const kWhite = new King(PieceColor.White, PieceType.King, new Square(1, 2));
+    const rWhite = new Rook(PieceColor.White, PieceType.Rook, new Square(2, 1));
+    const kBlack = new King(PieceColor.Black, PieceType.King, new Square(0, 0));
+    const board = new Board();
+    board.addPiece(kWhite);
+    board.addPiece(rWhite);
+    board.addPiece(kBlack);
+    const game = new Game(undefined, PieceColor.White, undefined, undefined, undefined, board);
+    // when
+    const checkMateMove = new Move(new Square(2,1), new Square(2,0), rWhite, null);
+    // then
+    const gameResult = game.isCheckmated();
+    expect(gameResult).toBe(true);
   });
 
   //TEST 2//
 
-  test("check white King checkmate on position [5][2]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(5, 2);
-    const blackKingPosition = new Square(7, 5);
-    const blackKnightPosition = new Square(6, 1);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(true);
+  test("BLACK KING CHECKMATE BY ROOK AND QUEEN", () => {
+    //given
+    const qWhite = new Queen(PieceColor.White, PieceType.Queen, new Square(1, 2));
+    const rWhite = new Rook(PieceColor.White, PieceType.Rook, new Square(2, 1));
+    const kBlack = new King(PieceColor.Black, PieceType.King, new Square(0, 0));
+    const board = new Board();
+    board.addPiece(qWhite);
+    board.addPiece(rWhite);
+    board.addPiece(kBlack);
+    const game = new Game(undefined, PieceColor.White, undefined, undefined, undefined, board);
+    // when
+    const checkMateMove = new Move(new Square(2,1), new Square(0,1), rWhite, null);
+    // then
+    const gameResult = game.isCheckmated();
+    expect(gameResult).toBe(true);
   });
 
   //TEST 3//
 
-  test("check white King checkmate on position [0][0]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(0, 0);
-    const whiteBishopPosition = new Square(1, 1);
-
-    const blackKingPosition = new Square(7, 5);
-    const blackKnightPosition = new Square(6, 1);
-    const blackRookPosition = new Square(3, 1);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const whiteBishop = new Bishop(
-      PieceColor.White,
-      PieceType.Bishop,
-      whiteBishopPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    const blackRook = new Rook(
-      PieceColor.Black,
-      PieceType.Rook,
-      blackRookPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteBishop);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-    newBoard.addPiece(blackRook);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(true);
+  test("BLACK KING IS NOT CHECKAMTED, BUT SHOULD BE CHECKED", () => {
+    //given
+    const kWhite = new King(PieceColor.White, PieceType.King, new Square(7, 7));
+    const rWhite = new Rook(PieceColor.White, PieceType.Rook, new Square(2, 1));
+    const kBlack = new King(PieceColor.Black, PieceType.King, new Square(0, 0));
+    const board = new Board();
+    board.addPiece(kWhite);
+    board.addPiece(rWhite);
+    board.addPiece(kBlack);
+    const game = new Game(undefined, PieceColor.White, undefined, undefined, undefined, board);
+    // when
+    const checkMateMove = new Move(new Square(2,1), new Square(2,0), rWhite, null);
+    // then
+    const checkMateResult = game.isCheckmated();
+    const checkResult = game.isChecked();
+    expect(checkMateResult).toBe(false);
+    expect(checkResult).toBe(true);
   });
 
   //TEST 4//
 
-  test("check white King checkmate on position [2][2]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(2, 2);
-    const whiteBishopPosition = new Square(3, 4);
-    const whitePawnPosition = new Square(7, 8);
-
-    const blackKingPosition = new Square(7, 5);
-    const blackKnightPosition = new Square(6, 1);
-    const blackRookPosition = new Square(3, 1);
-    const blackQueenPosition = new Square(5, 5);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const whiteBishop = new Bishop(
-      PieceColor.White,
-      PieceType.Bishop,
-      whiteBishopPosition
-    );
-
-    const whitePawn = new Pawn(
-      PieceColor.White,
-      PieceType.Pawn,
-      whitePawnPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    const blackRook = new Rook(
-      PieceColor.Black,
-      PieceType.Rook,
-      blackRookPosition
-    );
-
-    const blackQueen = new Queen(
-      PieceColor.Black,
-      PieceType.Queen,
-      blackQueenPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteBishop);
-    newBoard.addPiece(whitePawn);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-    newBoard.addPiece(blackRook);
-    newBoard.addPiece(blackQueen);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(true);
+  test("BLACK KING CHECKMATE BY TWO ROOKS", () => {
+    //given
+    const kWhite = new King(PieceColor.White, PieceType.King, new Square(7, 7));
+    const r1White = new Rook(PieceColor.White, PieceType.Rook, new Square(3, 1));
+    const r2White = new Rook(PieceColor.White, PieceType.Rook, new Square(2, 2));
+    const kBlack = new King(PieceColor.Black, PieceType.King, new Square(0, 0));
+    const board = new Board();
+    board.addPiece(r2White);
+    board.addPiece(r1White);
+    board.addPiece(kBlack);
+    board.addPiece(kWhite);
+    const game = new Game(undefined, PieceColor.White, undefined, undefined, undefined, board);
+    // when
+    const checkMateMove = new Move(new Square(2,2), new Square(2,0), r2White, null);
+    // then
+    const gameResult = game.isCheckmated();
+    expect(gameResult).toBe(true);
   });
-
-  //TEST 5//
-
-  test("check black King checkmate on position [6][5]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(3, 2);
-    const blackKingPosition = new Square(6, 5);
-    const blackKnightPosition = new Square(6, 4);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(false);
-  });
-
-  //TEST 6//
-  test("check black King checkmate on position [6][3]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(2, 2);
-    const whiteBishopPosition = new Square(3, 4);
-    const whitePawnPosition = new Square(7, 7);
-    const whiteRookPosition = new Square(3, 3);
-    const whiteQueenPosition = new Square(4, 4);
-
-    const blackKingPosition = new Square(6, 3);
-    const blackKnightPosition = new Square(6, 1);
-    const blackRookPosition = new Square(6, 4);
-    const blackQueenPosition = new Square(6, 5);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const whiteBishop = new Bishop(
-      PieceColor.White,
-      PieceType.Bishop,
-      whiteBishopPosition
-    );
-
-    const whitePawn = new Pawn(
-      PieceColor.White,
-      PieceType.Pawn,
-      whitePawnPosition
-    );
-
-    const whiteRook = new Rook(
-      PieceColor.White,
-      PieceType.Rook,
-      whiteRookPosition
-    );
-
-    const whiteQueen = new Pawn(
-      PieceColor.White,
-      PieceType.Queen,
-      whiteQueenPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    const blackRook = new Rook(
-      PieceColor.Black,
-      PieceType.Rook,
-      blackRookPosition
-    );
-
-    const blackQueen = new Rook(
-      PieceColor.Black,
-      PieceType.Queen,
-      blackQueenPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteBishop);
-    newBoard.addPiece(whitePawn);
-    newBoard.addPiece(whiteRook);
-    newBoard.addPiece(whiteQueen);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-    newBoard.addPiece(blackRook);
-    newBoard.addPiece(blackQueen);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(false);
-  });
-
-  //TEST 7//
-
-  test("check black King checkmate on position [6][6]", () => {
-    const newBoard = new Board();
-
-    const whiteKingPosition = new Square(5, 2);
-    const whiteQueenPosition = new Square(4, 4);
-    const whiteRookPosition = new Square(2, 3);
-    const blackKingPosition = new Square(6, 6);
-    const blackKnightPosition = new Square(6, 1);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const whiteQueen = new Pawn(
-      PieceColor.White,
-      PieceType.Queen,
-      whiteQueenPosition
-    );
-
-    const whiteRook = new Pawn(
-      PieceColor.White,
-      PieceType.Rook,
-      whiteRookPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    const blackKnight = new Knight(
-      PieceColor.Black,
-      PieceType.Knight,
-      blackKnightPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(whiteQueen);
-    newBoard.addPiece(whiteRook);
-    newBoard.addPiece(blackKing);
-    newBoard.addPiece(blackKnight);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(false);
-  });
-
-  //TEST 8//
-
-  test("check black King checkmate on position [6][6]", () => {
-    const newBoard = new Board();
-    const whiteKingPosition = new Square(3, 4);
-    const blackKingPosition = new Square(6, 6);
-
-    const whiteKing = new King(
-      PieceColor.White,
-      PieceType.King,
-      whiteKingPosition
-    );
-
-    const blackKing = new King(
-      PieceColor.Black,
-      PieceType.King,
-      blackKingPosition
-    );
-
-    newBoard.addPiece(whiteKing);
-    newBoard.addPiece(blackKing);
-
-    const newGame = new Game(
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      newBoard
-    );
-
-    const result = newGame.isCheckmated();
-    expect(result).toBe(false);
-  });
+ 
 });
