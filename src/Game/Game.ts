85ab11c580abd908c1796831fddc9e918290dcda
Joanna <joanna.kilar.jk@gmail.com>
2021-02-13 14:17:57 +0100

    roszada

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -81,16 +81,18 @@ export class Game {
     this._board = board;
   }
 
-  addMove(move: Move): void { }
+  addMove(move: Move): void {
+    this._playedMoves.push(move);
+  }
 
   isEnded(): boolean {
-      if ((this.isStalemated() == true) || (this.isCheckmated() == true) || (this.checkTripleMoveRule() == true) ||
-        (this.checkFiftyMoveRule() == true)) {
-        return true;
-      } else {
-        return false;
-      }
+    if ((this.isStalemated() == true) || (this.isCheckmated() == true) || (this.checkTripleMoveRule() == true) ||
+      (this.checkFiftyMoveRule() == true)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
   isChecked(): boolean {
     return true;
@@ -101,22 +103,107 @@ export class Game {
   isStalemated(): boolean {
     return true;
   }
+
   getAvailableSquares(piece: Piece): Square[] {
-    let allSquares:Square[] = piece.validMoves();
-    let availableSquares:Square[] = [];
+    let allSquares: Square[] = piece.validMoves();
+    let availableSquares: Square[] = [];
     availableSquares.forEach((e) => {
-      if(this.isMovePossible(new Move(piece.placeAt, e, piece, null))){
+      if (this.isMovePossible(new Move(piece.placeAt, e, piece, null))) {
         availableSquares.push(e);
       }
     })
-    
+
     return availableSquares;
   }
 
-  isCastlePossible(): boolean { 
+  isSquareAttacked(square: Square): boolean {
     return true;
-   }
+  }
 
+  isCastlePossible(move: Move): boolean { //czy roszada jest możliwa
+    const pieceType = move.piece.pieceType;
+    const pieceColor = move.piece.pieceColor;
+    const start = move.startSquare;
+    const end = move.endSquare;
+    const piecesArray = this._board.squares;
+    const playedMoves = this._playedMoves;
+
+    if (pieceType == PieceType.King) { //czy to na pewno król
+      for (let i = 0; i < playedMoves.length; i++) { //czy król, którym próbujemy zrobić roszadę się ruszył
+        if (playedMoves[i].piece.pieceType == PieceType.King && playedMoves[i].piece.pieceColor == pieceColor) {
+          return false;
+        }
+
+        //którą roszadę chcemy wykonać(długą czy krótką) i dla jakiego koloru
+        if (start == new Square(7, 4) && end == new Square(7, 2)) {//długa roszada dla białych = czy 7,1 7,2 i 7,3 jest puste, a 7,2 i 7,3 wolne od szacha
+          if (isRookMoved(new Square(7, 0), PieceColor.White) == true) {  //czy wieża z którą chcemy zrobić roszadę się ruszyła
+            return false;
+          } else if (isSquaresEmpty([new Square(7, 1), new Square(7, 2), new Square(7, 3)]) // jeżeli wieża z którą chcemy zrobić roszadę się nie ruszyła, to sprawdzamy, czy pola pomiędzy królem a tą wieżą są puste i czy nie są atakowane
+            && this.isSquareAttacked(new Square(7, 2)) == false
+            && this.isSquareAttacked(new Square(7, 3)) == false) {
+            return true;
+          }
+        } else if (start == new Square(7, 4) && end == new Square(7, 6)) {//krótka roszada dla białych = czy 7,5 i 7,6 jest puste i wolne od szacha
+          if (isRookMoved(new Square(7, 7), PieceColor.White) == true) { //czy wieża z którą chcemy zrobić roszadę się ruszyła
+            return false;
+          } else if (isSquaresEmpty([new Square(7, 5), new Square(7, 6)])
+            && this.isSquareAttacked(new Square(7, 5)) == false
+            && this.isSquareAttacked(new Square(7, 6)) == false) {
+            return true;
+          }
+        } else if (start == new Square(0, 4) && end == new Square(0, 2)) {//długa roszada dla czarnych = czy 0,1 0,2 i 0,3 jest puste i czy 0,2 i 0,3 jest wolne od szacha
+          if (isRookMoved(new Square(0, 0), PieceColor.Black) == true) { //czy wieża z którą chcemy zrobić roszadę się ruszyła
+            return false;
+          } else if (isSquaresEmpty([new Square(0, 1), new Square(0, 2), new Square(0, 3)])
+            && this.isSquareAttacked(new Square(0, 2)) == false
+            && this.isSquareAttacked(new Square(0, 3)) == false) {
+            return true;
+          }
+        } else if (start == new Square(0, 4) && end == new Square(0, 6)) {//krótka roszada dla czarnych = czy 0, 5 i 0, 6 jest puste i wolne od szacha
+          if (isRookMoved(new Square(0, 7), PieceColor.Black) == true) { //czy wieża z którą chcemy zrobić roszadę się ruszyła
+            return false;
+          } else if (isSquaresEmpty([new Square(0, 5), new Square(0, 6)])
+            && this.isSquareAttacked(new Square(0, 5)) == false
+            && this.isSquareAttacked(new Square(0, 6)) == false) {
+            return true; {
+            }
+          }
+        }
+      }
+
+      function isRookMoved(square: Square, rookPieceColor: PieceColor): boolean { //funkcja pomocnicza
+        for (let i = 0; i < playedMoves.length; i++) {
+          if (playedMoves[i].piece.pieceType == PieceType.Rook && playedMoves[i].piece.pieceColor == rookPieceColor && playedMoves[i].startSquare == square) {
+            return true;
+          }
+        }
+        return false;
+      }
+
+      function isSquaresEmpty(squares: Square[]) { //funkcja pomocnicza
+        if (squares.length == 3) {
+          for (let i = 0; i < piecesArray.length; i++) {
+            for (let j = 0; j < piecesArray[i].length; j++) {
+              if (piecesArray[i][j].placeAt != squares[0] && piecesArray[i][j].placeAt != squares[1] && piecesArray[i][j].placeAt != squares[2]) {
+                return true;
+              }
+            }
+          }
+          return false;
+        } else if (squares.length == 2) {
+          for (let i = 0; i < piecesArray.length; i++) {
+            for (let j = 0; j < piecesArray[i].length; j++) {
+              if (piecesArray[i][j].placeAt != squares[0] && piecesArray[i][j].placeAt != squares[1]) {
+                return true;
+              }
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return false;
+  }
 
   checkFiftyMoveRule(): boolean { //czy zasada 50 ruchów bez bicia lub ruchu pionem jest prawdziwa
     const movesArray = this.playedMoves;
@@ -136,41 +223,41 @@ export class Game {
     return true;
   }
   pawnPromotion(): Piece {
-    return new King(PieceColor.Black, PieceType.King, new Square(6,6));
+    return new King(PieceColor.Black, PieceType.King, new Square(6, 6));
   }
 
-  isMovePossiblePawn(move: Move): boolean{return true;}
-  isMovePossibleRook(move: Move): boolean{return true;}
-  isMovePossibleKnight(move: Move): boolean{return true;}
-  isMovePossibleBishop(move: Move): boolean{return true;}
-  isMovePossibleKing(move: Move): boolean{return true;}
-  isMovePossibleQueen(move: Move): boolean{return true;}
+  isMovePossiblePawn(move: Move): boolean { return true; }
+  isMovePossibleRook(move: Move): boolean { return true; }
+  isMovePossibleKnight(move: Move): boolean { return true; }
+  isMovePossibleBishop(move: Move): boolean { return true; }
+  isMovePossibleKing(move: Move): boolean { return true; }
+  isMovePossibleQueen(move: Move): boolean { return true; }
 
-  
+
   isMovePossible(move: Move): boolean {
     let pieceType = move.piece.pieceType;
     switch (pieceType) {
       case PieceType.Pawn:
         return this.isMovePossiblePawn(move);
-        
+
       case PieceType.Rook:
         return this.isMovePossibleRook(move);
-        
+
       case PieceType.Knight:
         return this.isMovePossibleKnight(move);
-        
+
       case PieceType.Bishop:
         return this.isMovePossibleBishop(move);
-        
+
       case PieceType.King:
         return this.isMovePossibleKing(move);
-        
+
       case PieceType.Queen:
         return this.isMovePossibleQueen(move);
-        
+
       default:
-        return false;    
+        return false;
     }
-     //sprawdza czy nowy ruch - endSquare:Square z klasy Move, znajduje sie w tablicy, którą zwraca getAvailableSquares(square: Square): Square[]
+    //sprawdza czy nowy ruch - endSquare:Square z klasy Move, znajduje sie w tablicy, którą zwraca getAvailableSquares(square: Square): Square[]
   }
 }
